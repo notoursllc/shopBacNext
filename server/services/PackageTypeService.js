bd0f706c879bb9a48d705a4308c1d095a3ff6a8c
gbru <gbru@users.noreply.github.com>
2023-03-20 22:23:14 -0700

    feat: Add addVirtuals to PackageTypeService

--- server/services/PackageTypeService.js
+++ server/services/PackageTypeService.js
@@ -1,11 +1,27 @@
 import Joi from 'joi';
 import PackageTypeModel from '../models/PackageTypeModel.js';
 import BaseService from './BaseService.js';
+import { makeArray } from '../utils/index.js';
 
 export default class PackageTypeService extends BaseService {
 
     constructor() {
         super(new PackageTypeModel());
+    }
+
+
+    addVirtuals(data) {
+        makeArray(data).forEach((PackageType) => {
+            PackageType.volume_cm = (function(obj) {
+                const val = parseFloat(obj.length_cm || 0, 2)
+                    * parseFloat(obj.width_cm || 0, 2)
+                    * parseFloat(obj.height_cm || 0, 2);
+
+                return val.toFixed(2);
+            })(PackageType);
+        });
+
+        return data;
     }
 
 
